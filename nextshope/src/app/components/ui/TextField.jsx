e7e697537742db7d84d544610ef83e928d94b961
Ali Kashefi <alikashefi274@gmail.com>
2025-05-12 23:17:26 +0330

    feat: Creating a dynamic looking input
    -Flexibility in styling via props function
    Flexible label value
    
    - In case of error, a span tag with error text is placed below the input
    
    -Dynamic placeholder

---
+++ nextshope/src/app/components/ui/TextField.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function TextField({
+  rest,
+  type,
+  placeholder,
+  className,
+  onChange,
+  name,
+  error,
+  label,
+}) {
+  return (
+    <>
+      <label className="w-auto" htmlFor={name}>{label}</label>
+      <input
+        id={name}
+        type={type}
+        placeholder={placeholder}
+        className={`focus:shadow-secondary-50 outline-none w-lg sm:w-sm md:w-md h-12 bg-primary-50 rounded-md text-secondary-900 text-center ${className}`}
+        onChange={onChange}
+        {...rest}
+      />
+
+      {error && <span className="text-red-700">{error.message}</span>}
+    </>
+  );
+}
+
+export default TextField;
