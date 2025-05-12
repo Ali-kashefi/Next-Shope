e225a7835232ff5f1ba2270037e8979c1632e408
Ali Kashefi <alikashefi274@gmail.com>
2025-05-12 23:15:19 +0330

    feat: change style

--- nextshope/src/app/components/ui/Buttone.jsx
+++ nextshope/src/app/components/ui/Buttone.jsx
@@ -1,17 +1,13 @@
-const btntype = {
-    primary: "bg-primary-900  hover:bg-primary-600",
-    secondary: "bg-gray-400  rounded-md",
-    outline: "border border-blue-400 text-blue-400  rounded-md",
-    danger: "bg-red-500 text-white  rounded-md",
-  };
+
   
   function Button({ children, btnType, onClick, disabled, className, ...props }) {
     return (
       <button
-        className={`  ${btntype[btnType]} bg-primary-900  hover:bg-primary-600  rounded-md  ${className}`}
+        className={`   bg-primary-900  hover:bg-primary-600  rounded-md  ${className}`}
         onClick={onClick}
         disabled={disabled}
         {...props}
+        type={btnType}
       >
         {children}
       </button>
