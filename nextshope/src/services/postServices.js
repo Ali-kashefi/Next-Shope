282f86d00c45591f110a5d027270976a31b66951
Ali Kashefi <alikashefi274@gmail.com>
2025-05-16 02:03:45 +0330

    feat:add OTP Check API to check the sent code

--- nextshope/src/services/postServices.js
+++ nextshope/src/services/postServices.js
@@ -1,5 +1,8 @@
 import http from "./httpServices";
 
-export async function sendOTPAPI({ phoneNumber }) {
+export async function sendOTPAPI({ phoneNumber} ) {
     return http.post("/user/get-otp", { phoneNumber }).then((res) => res.data);
+}
+export async function cheackOTPAPI({ data} ) {
+    return http.post("/user/check-otp", { data }).then((res) => res.data);
 }
