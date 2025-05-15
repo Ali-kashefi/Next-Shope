3e8c2dc8d1276e9207e8a6cf99aedf82a69bd56a
Ali Kashefi <alikashefi274@gmail.com>
2025-05-16 02:02:44 +0330

    feat: Add React Query Provider for state management
    - Implement `QueryClientProvider` to manage global queries
    - Initialize `QueryClient` using `useState` for persistence
    - Ensure efficient state handling for API requests
    - Wrap application components inside the provider for accessibility

---
+++ nextshope/src/app/Provider.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+
+export default function Provider({ children }) {
+ 
+  const [queryClient] = React.useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
