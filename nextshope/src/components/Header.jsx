9dae5960a9ea1368b9c775b0b213996d5bbba043
Ali Kashefi <alikashefi274@gmail.com>
2025-05-20 19:27:03 +0330

    feat:add header to display in layout -Blur page if loading -Show user name if user exists

---
+++ nextshope/src/components/Header.jsx
@@ -0,0 +1,69 @@
+"use client";
+import { getUserProfileAPI } from "@/service/postServices";
+import { ShoppingCartIcon } from "@heroicons/react/24/solid";
+import { useQuery } from "@tanstack/react-query";
+import Link from "next/link";
+import React from "react";
+
+function Header() {
+  const { error, isLoading, data } = useQuery({
+    queryKey: ["user"],
+    queryFn: getUserProfileAPI,
+    retry: false,
+    refetchOnWindowFocus: true,
+  });
+  console.log(data, error, isLoading);
+  const { user, cart } = data || {};
+
+  return (
+    <>
+     <header
+      className={`shadow-md mb-10 sticky top-0 transition-all duration-200 bg-white px-12 ${
+        isLoading ? "blur-sm opacity-70" : "opacity-100 blur-0"
+      }`}
+    >
+      <nav>
+        <ul className="flex items-center  justify-between py-2 container xl:max-w-screen-xl">
+          <li>
+            <Link className="block py-2" href="/">
+              خانه
+            </Link>
+          </li>
+          <li>
+            <Link className="block py-2" href="/products">
+              محصولات
+            </Link>
+          </li>
+          <li>
+            <Link className="block py-2" href="/profile">
+              پنل کاربر
+            </Link>
+          </li>
+          <li>
+            <Link className="block py-2" href="/admin">
+              پنل ادمین
+            </Link>
+          </li>
+          <li>
+            <Link className="block py-2" href="/cart">
+              سبد خرید ({cart ? cart.payDetail.productIds.length : 0})
+            </Link>
+          </li>
+          {user ? (
+            <span>{user.name}</span>
+          ) : (
+            <li>
+              <Link className="block py-2" href="/auth">
+                ورود
+              </Link>
+            </li>
+          )}
+        </ul>
+      </nav>
+    </header>
+
+    </>
+  );
+}
+
+export default Header;
