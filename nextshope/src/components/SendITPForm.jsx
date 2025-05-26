99d3c2599f31288a6fb59eb1f01bd4c39b9acf5c
Ali Kashefi <alikashefi274@gmail.com>
2025-05-26 19:40:19 +0330

    feat(ui): Create `SendOTPForm` component for phone number input
    This commit introduces the `SendOTPForm` component, designed for collecting a phone number and initiating an OTP (One-Time Password) request.
    
    - Integrates `TextField` for mobile number input and `Button` for submission.
    - Manages `phoneNumber` state, `onChange`, `onSubmit` handlers, and `isLoading` state.
    - Provides clear placeholders and a "Send" button.

---
+++ nextshope/src/components/SendITPForm.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Button from "@/components/ui/Buttone";
+import TextField from "@/components/ui/TextField";
+
+function SendITPForm({ phoneNumber, onChange, onSubmit, isLoading,rest}) {
+  return (
+    <form onSubmit={onSubmit}>
+      <div className="flex flex-col gap-4 items-center">
+        <TextField
+          label="شماره موبایل"
+          placeholder="شماره موبایل را وارد کنید"
+          name="mobile"
+          type="text"
+          onChange={onChange}
+          value={phoneNumber}
+          {...rest}
+        />
+        { isLoading && <p>loading</p>}
+        <Button
+          {...rest}
+          btnType="submit"
+          className="w-lg sm:w-sm md:w-md h-12"
+        >
+          ارسال
+        </Button>
+      </div>
+    </form>
+  );
+}
+
+export default SendITPForm;
