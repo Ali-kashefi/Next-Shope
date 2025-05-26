2a50fe55fa46528c037307c260328c74bbf077dd
Ali Kashefi <alikashefi274@gmail.com>
2025-05-26 19:25:00 +0330

    fix(middleware): Correct user authentication and role-based redirects
    This commit refactors the `middleware` to properly handle user authentication and role-based access for `/profile` and `/admin` routes.
    
    - Ensures unauthorized users are redirected to `/signup` when accessing `/profile` or `/admin`.
    - Redirects non-admin users from `/admin` to the homepage.
    - Fixes the `matcher` configuration to correctly apply the middleware to the intended paths.

---
+++ nextshope/src/middleware.js
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import middlewareAuth from "utils/middlewareAut";
+
+
+export async function middleware(req) {
+  const url = req.url;
+  const pathname = req.nextUrl.pathname;
+
+
+  if (pathname.startsWith("/profile")) {
+    const user = await middlewareAuth(req);
+    if (!user) return NextResponse.redirect(new URL("/signup", url));
+  }
+
+  if (pathname.startsWith("/admin")) {
+    const user = await middlewareAuth(req);
+    if (!user) return NextResponse.redirect(new URL("/signup", url));
+    if (user && user.role !== "ADMIN")
+      return NextResponse.redirect(new URL("/", req.url));
+  }
+}
+
+export const config = {
+  matcher: ["/dashbord/:path*", "/profile/:path*"],
+};
