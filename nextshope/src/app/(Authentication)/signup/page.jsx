6a5834126c146ea8895376bc93ce7fffb17a86d4
Ali Kashefi <alikashefi274@gmail.com>
2025-08-14 00:49:46 +0330

    feat: handle user redirection after OTP verification
    Redirect authenticated users to the home page and new users to the profile completion page.

--- nextshope/src/app/(Authentication)/signup/page.jsx
+++ nextshope/src/app/(Authentication)/signup/page.jsx
@@ -48,29 +48,35 @@ function page() {
       setStep(2);
       setTime(TIMER_BASE);
       setOtp("");
-
     } catch (err) {
       toast.error(error.data.message);
     }
   };
 
   const cheackOTP = async (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    if (!otp || otp.length < 6) {
-      toast.error("Invalid OTP code!");
-      return;
+  if (!otp || otp.length < 6) {
+    toast.error("کد نامعتبر است");
+    return;
+  }
+
+  try {
+    const response = await otpmutate({ otp, phoneNumber });
+
+    if (response.isActive) {
+      toast.success("کد تایید شد به نکست شاپ خوش آمدید");
+      router.push("/"); 
+    } else {
+      toast.success("کد تایید شد. لطفاً اطلاعات پروفایل خود را تکمیل کنید.");
+      router.push("/complete_profile"); 
     }
 
-    try {
-      const response = await otpmutate({ otp, phoneNumber });
-      toast.success(response.message);
-
-      response.isActive ? router.push("/") : router.push("/complete_profile");
-    } catch (error) {
-      console.log(error?.response.data.message);
-    }
-  };
+  } catch (error) {
+    toast.error("کد اشتباه است");
+    console.error("خطا در بررسی OTP:", error); 
+  }
+};
 
   const onBack = (e) => {
     e.preventDefault();
